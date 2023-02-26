c4424b4a91860fd2f92ebb57105fd4580a36a7d7
GordonOtieno <otienogordon95@gmail.com>
2023-02-26 21:46:25 +0300

    update cars controllers

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,15 +1,40 @@
 class Api::V1::CarsController < ApplicationController
   def index
     @cars= Car.all
-    render json: @cars;
+    if @cars.empty?
+      render json: { message: "No cars found" }
+    else
+      render json: @cars;
+    end
+    
   end
 
-  def new
-  end
+  def show
+    @car = Car.find(params[:id])
+    render json: @car
+    end
 
   def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      render json: { message: 'Car was successfully created'},status: :created
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @car = Car.find(params[:id]);
+    if @car.destroy
+      render json: { message: 'Car was successfully deleted'}, status: :no_content
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def car_params
+    params.require(:car).permit(:name, :car_type, :make, :description, :image, :cost, :speed, :color)
   end
 end
