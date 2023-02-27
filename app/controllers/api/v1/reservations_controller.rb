db0b342d2d1c9dbc916b29706fa78883146d395b
Amine <ouas.amine@outlook.com>
2023-02-27 15:40:15 +0100

    Implement resevations create action

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -2,11 +2,18 @@ class Api::V1::ReservationsController < ApplicationController
     def index
     end
     def create
-      @new_reservation = Reservation.new
-    end
-    def show
-    end
-    def destroy
+      @new_reservation = Reservation.new(
+        date: params[:date],
+        city: params[:city],
+        user_id: params[:user_id],
+        car_id: params[:car_id]
+      )
+
+      if @new_reservation.save
+        render json: "Resevation Created!", status: :ok
+      else
+        render json: "Failed!", status: :unprocessable_entity
+      end
     end
     
   end
