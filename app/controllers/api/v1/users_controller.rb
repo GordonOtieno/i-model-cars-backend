32f58b96d9dc502676613fe5c7e6bcda016ef1a5
Amine <ouas.amine@outlook.com>
2023-02-27 15:26:35 +0100

    Add user id to response body & use response codes parameters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,24 +6,19 @@ class Api::V1::UsersController < ApplicationController
       )
 
       if @new_user.save
-        render json: "User created successfully!"
-        response.status = 200
+        render json: "User created successfully!", status: :ok
       else
-        render json: "Failed!"
-        response.status = 400
+        render json: "Failed!", status: :unprocessable_entity
       end
     end
 
     def show
-      p params
       if User.exists?(name: params[:user_name])
         @user = User.find_by(name: params[:user_name])
-        @resp = {name: @user.name, email: @user.email}
-        render json: @resp
-        response.status = 200
+        @resp = {id:@user.id, name: @user.name, email: @user.email}
+        render json: @resp, status: :ok
       else
-        render json: "User not found!"
-        response.status = 404
+        render json: "User not found!", status: :not_found
       end
     end
   end
