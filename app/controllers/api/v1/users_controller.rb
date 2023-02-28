81745d5127ed7c3ebc96e38e06e22dc9f6d7efd3
GordonOtieno <otienogordon95@gmail.com>
2023-02-28 10:39:36 +0300

    Solved linters errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,22 +11,10 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: 'Failed!'
       response.status = 400
-      render json: 'User created successfully!', status: :ok
-    else
-      render json: 'Failed!', status: :unprocessable_entity
     end
   end
 
   def show
-    p params
-    if User.exists?(name: params[:user_name])
-      @user = User.find_by(name: params[:user_name])
-      @resp = { name: @user.name, email: @user.email }
-      render json: @resp
-      response.status = 200
-    else
-      render json: 'User not found!'
-      response.status = 404
     if User.exists?(name: params[:user_name])
       @user = User.find_by(name: params[:user_name])
       @resp = { id: @user.id, name: @user.name, email: @user.email }
